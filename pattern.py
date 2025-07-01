0eaf3edf3cf83bec7bc8b7228babe8966f1452d9
JaySurya <jaysurya6969@gmail.com>
2025-07-01 19:50:26 +0530

    name change

--- pattern.py
+++ pattern.py
@@ -1,13 +1,10 @@
-# Number of rows for the pattern
-n = 5
-
-#inverted pattern
-# Outer loop for rows
-for i in range(n, 0, -1):
-    # Inner loop for printing stars
-    print('*' * i)
+n=int(input("Enter how many lines you want to print"))
+for i in range(0,n):
+    for j in range(0,i+1):
+        print(j,end=" ")
+    print()
 
 
-#normal pattern
-for i in range(0,n+1,+1):
-    print('*'*i)
+   
+
+        
